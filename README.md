e44f98566e1adfb2e1c5e131b28f4356f7d01422
KristaTheCoder <kshuckerow@gmail.com>
2018-02-03 14:37:29 -0500

    Working xBar chart based on diana thingy

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # stats
-Quality Control Graphs and Analytics. 
+Quality Control Graphs and Analytics.
+
+
+USE AN EWMA chart because we're using her entire history. for perminant variation
+
+For the positive negative you can do n and np charts
+x bar and s charts for the remainder of the charts
+
+c control charts for the actual scores, lots of opportunities for it to occur.
+
+Use an Operating Characteristic Curve to get the probability for finding something wrong in the process for the scores after a given number.
+
+Cumulative sum (cusum) - to see if the process
--- qqc.py
+++ qqc.py
@@ -1,28 +1,25 @@
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 import time
+
 qcc = importr("qcc")
 r = robjects.r
-x = robjects.FloatVector([5,10,20,30])
-
 
 
 #R part
 robjects.r('''
-  data(pistonrings)
-  diameter <- with(pistonrings,qcc.groups(diameter, sample))
-  head(diameter)
+MyData <- read.csv(file="data_sample.csv", header = FALSE, sep = ",")
+myData <- MyData[1:30,]
+
+a <- myData$V3
+b <- myData$V1
+V3 <- qcc.groups(myData$V3, myData$V1)
+q1 <- qcc(V3, type="xbar")
 
 
- q1 <- qcc(diameter[1:25,], type="xbar", newdata=diameter[26:40,])
- y <- plot(q1, chart.all=FALSE)
  ''')
 
-r_y = robjects.globalenv['q1']
-r.plot( r_y)
-print(r.summary(r_y))
+r_data = robjects.globalenv['q1']
+r.plot(r_data)
+
 time.sleep(20)
-
-
-#r_runExample = robjects.globalenv['y']
-#print(r_runExample)
